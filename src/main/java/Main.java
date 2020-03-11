04a59a741ca13f9b77f1bf1a307d7992aba26ff0
Andrey Ovchinnikov <dream.of.yaz@gmail.com>
2020-03-12 01:50:34 +0600

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,57 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Input data:100 200
+ * Output data:100 200 125
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        File file_input = new File("./src/main/resources/input.txt");
+
+        Scanner scanner = new Scanner(file_input);
+
+        int start = scanner.nextInt();
+        int end = scanner.nextInt();
+
+        int count;
+        int max_count = 0;
+
+        for (int i = start; i <= end; i++) {
+            int num = i;
+            count = 0;
+
+            while (num != 1) {
+                count++;
+                num = sequence(num);
+            }
+            count++;
+
+            if (max_count < count) {
+                max_count = count;
+            }
+        }
+        File file_output = new File("./src/main/resources/output.txt");
+
+        if(file_output.createNewFile()){
+            System.out.println("File created");
+        }
+
+        try(FileWriter writer = new FileWriter(file_output, false)){
+            writer.write(String.format("%d %d %d", start, end, max_count));
+
+            writer.flush();
+        }
+    }
+
+    private static int sequence(int num) {
+        if ((num & 1) == 0) {
+            return num >> 1;
+        } else {
+            return num * 3 + 1;
+        }
+    }
+}
